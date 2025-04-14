3fc982e1cb38da764835c55d8f9d6cba95e4ebe5
Dizaled <Silifslikirill@gmail.com>
2025-04-15 01:20:02 +0700

    Json

--- src/main/java/org/example/mrdverkin/security/RegistrationController.java
+++ src/main/java/org/example/mrdverkin/security/RegistrationController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 
 @Controller
 @RequestMapping("/register")
@@ -19,11 +21,13 @@ public class RegistrationController {
     private PasswordEncoder passwordEncoder;
 
     @PostMapping
-    public ResponseEntity<String> processRegistration(@RequestBody RegistrationForm form) {
+    public ResponseEntity<Map<String, String>>processRegistration(@RequestBody RegistrationForm form) {
         if (!form.isPasswordMatching()){
-            return ResponseEntity.ok("Failed registration");
+            return ResponseEntity.ok(Map.of("message", "Failed registration"));
+
         }
         userRepo.save(form.toUser(passwordEncoder));
-        return ResponseEntity.ok("Register Successful");
+        return ResponseEntity.ok(Map.of("message", "Login Successful"));
+
     }
 }
