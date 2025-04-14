89ed87f387587da64bc07ecaee56a13b802a710e
Dizaled <Silifslikirill@gmail.com>
2025-04-14 21:09:57 +0700

    RegistrationController update

--- src/main/java/org/example/mrdverkin/controllers/api/AuthController.java
+++ src/main/java/org/example/mrdverkin/controllers/api/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-//@RequiredArgsConstructor
+
 public class AuthController {
 
     private final AuthService authService;
--- src/main/java/org/example/mrdverkin/security/RegistrationController.java
+++ src/main/java/org/example/mrdverkin/security/RegistrationController.java
@@ -2,10 +2,12 @@ package org.example.mrdverkin.security;
 
 import org.example.mrdverkin.dataBase.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -17,21 +19,16 @@ public class RegistrationController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-//    public RegistrationController(
-//            UserRepository userRepo, PasswordEncoder passwordEncoder) {
-//        this.userRepo = userRepo;
-//        this.passwordEncoder = passwordEncoder;
-//    }
     @GetMapping
     public String registerForm() {
         return "registration";
     }
     @PostMapping
-    public String processRegistration(RegistrationForm form) {
+    public ResponseEntity<String> processRegistration(@RequestBody RegistrationForm form) {
         if (!form.isPasswordMatching()){
-            return "registration";
+            return ResponseEntity.ok("Failed registration");
         }
         userRepo.save(form.toUser(passwordEncoder));
-        return "redirect:/login";
+        return ResponseEntity.ok("Register Successful");
     }
 }
