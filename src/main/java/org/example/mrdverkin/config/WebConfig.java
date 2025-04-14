50c837cfd6ae5d0e139c0a84bc3d1d5b17e5b3cd
Dizaled <Silifslikirill@gmail.com>
2025-04-15 01:37:41 +0700

    Json

--- src/main/java/org/example/mrdverkin/config/WebConfig.java
+++ src/main/java/org/example/mrdverkin/config/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:8081")
+                        .allowedOrigins("http://fast-door.ru")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
