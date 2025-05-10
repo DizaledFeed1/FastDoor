fea82c69b3fb72bac4eda54462296556164ce7fb
Dizaled <Silifslikirill@gmail.com>
2025-05-10 22:37:10 +0700

    update api

--- src/main/java/org/example/mrdverkin/controllers/OrderController.java
+++ src/main/java/org/example/mrdverkin/controllers/OrderController.java
@@ -1,34 +1,34 @@
-package org.example.mrdverkin.controllers;
-
-import org.example.mrdverkin.dataBase.Entitys.Order;
-import org.example.mrdverkin.dto.OrderAttribute;
-import org.example.mrdverkin.services.OrderService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-
-@Controller
-@RequestMapping("/edit")
-public class OrderController {
-    @Autowired
-    private OrderService orderService;
-
-
-    @GetMapping("/{id}")
-    public String edit(@PathVariable Long id, Model model,
-                       @AuthenticationPrincipal UserDetails userDetails) {
-        System.out.println(userDetails.getAuthorities());
-        Order order = orderService.findOrderById(id);
-        OrderAttribute orderAttribute = new OrderAttribute().fromOrder(order);
-        model.addAttribute("orderAttribute", orderAttribute);
-        model.addAttribute("role",userDetails.getAuthorities());
-        return "editOrder";
-    }
-
+//package org.example.mrdverkin.controllers;
+//
+//import org.example.mrdverkin.dataBase.Entitys.Order;
+//import org.example.mrdverkin.dto.OrderAttribute;
+//import org.example.mrdverkin.services.OrderService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.annotation.AuthenticationPrincipal;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.web.bind.annotation.*;
+//
+//@Controller
+//@RequestMapping("/edit")
+//public class OrderController {
+//    @Autowired
+//    private OrderService orderService;
+//
+//
+//    @GetMapping("/{id}")
+//    public String edit(@PathVariable Long id, Model model,
+//                       @AuthenticationPrincipal UserDetails userDetails) {
+//        System.out.println(userDetails.getAuthorities());
+//        Order order = orderService.findOrderById(id);
+//        OrderAttribute orderAttribute = new OrderAttribute().fromOrder(order);
+//        model.addAttribute("orderAttribute", orderAttribute);
+//        model.addAttribute("role",userDetails.getAuthorities());
+//        return "editOrder";
+//    }
+//
 //    @PostMapping("/{id}")
 //    public String updateOrder(@PathVariable Long id, @ModelAttribute("orderAttribute") OrderAttribute orderAttribute, BindingResult bindingResult) {
 //        if (orderService.updateOrder(id, orderAttribute,bindingResult).hasErrors()){
@@ -36,4 +36,4 @@ public class OrderController {
 //        }
 //        return "redirect:/listOrdersSeller";
 //    }
-}
+//}
