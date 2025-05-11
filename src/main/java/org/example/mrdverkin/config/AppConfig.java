743995829461ff4dad75299c248dc565f6124a34
Dizaled <Silifslikirill@gmail.com>
2025-05-11 13:06:44 +0700

    documentation add

--- src/main/java/org/example/mrdverkin/config/AppConfig.java
+++ src/main/java/org/example/mrdverkin/config/AppConfig.java
@@ -1,10 +1,19 @@
 package org.example.mrdverkin.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Fast Door API",
+                version = "v1.0",
+                description = "API for managing orders, sellers, and customers"
+        )
+)
 public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
