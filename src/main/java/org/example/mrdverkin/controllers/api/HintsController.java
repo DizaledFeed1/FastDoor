400a47958059974a1908ff9c64862e5180dc0709
Dizaled <Silifslikirill@gmail.com>
2025-09-19 14:56:35 +0700

    update

--- src/main/java/org/example/mrdverkin/controllers/api/HintsController.java
+++ src/main/java/org/example/mrdverkin/controllers/api/HintsController.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.mrdverkin.dataBase.Entitys.User;
 import org.example.mrdverkin.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
